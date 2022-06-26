9d5bb70e00fa772b3a4bfd768011b55c5d8333e7
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-06-26 03:09:50 +0100

    basic functionality

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+let firstValue = prompt('Enter first value: ');
+let secondValue = prompt('Enter second value: ');
+let operator = prompt('Enter operator: ');
+let result = 0;
 
+if (operator === '+') {
+    console.log(`${firstValue} + ${secondValue} is ${parseInt(firstValue + secondValue)}`);
+} else if (operator === '-') {
+    console.log(firstValue - secondValue);
+} else if (operator === '*') {
+    console.log(firstValue * secondValue);
+} else if (operator === '/') {   
+    console.log(firstValue / secondValue);
+}
